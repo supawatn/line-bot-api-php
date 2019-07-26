68aa8e73ef5131686b8bfefcbf0edae0c529a372
supawatn <53294417+supawatn@users.noreply.github.com>
2019-07-26 09:52:19 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "DeviceKey/"; //enter your appid
+		$KEY = "sg4OtsxnNrFY1ET"; //enter your web key
+		$SECRET = "B7ocuvDCQCw2ryV3nJs9msDFo"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
